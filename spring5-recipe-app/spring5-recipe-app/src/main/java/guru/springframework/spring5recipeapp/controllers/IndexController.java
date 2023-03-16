cb7e1ccb54f88f107174330101e173530fef0371
FernandoArellano <fer_america20@outlook.com>
2023-03-16 12:40:05 -0600

    added project lombok

--- spring5-recipe-app/spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
+++ spring5-recipe-app/spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
@@ -5,10 +5,12 @@ import guru.springframework.spring5recipeapp.domain.UnitOfMeasure;
 import guru.springframework.spring5recipeapp.repositories.CategoryRepository;
 import guru.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import guru.springframework.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -20,6 +22,7 @@ public class IndexController {
 
     @RequestMapping({"","/","/index"})
     public String getIndexPage(Model model){
+        log.debug("Getting index page");
 
         model.addAttribute("recipes", recipeService.getRecipes());
 
