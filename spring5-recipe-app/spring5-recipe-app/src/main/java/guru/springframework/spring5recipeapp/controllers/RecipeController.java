69fa1d32af2b9ce2883c6c9ae08795f2ac0acfe0
FernandoArellano <fer_america20@outlook.com>
2023-03-27 13:55:27 -0600

    added number format exception template, controller and unit test

--- spring5-recipe-app/spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/controllers/RecipeController.java
+++ spring5-recipe-app/spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/controllers/RecipeController.java
@@ -63,7 +63,7 @@ public class RecipeController {
     @ExceptionHandler(NotFoundException.class)
     public ModelAndView handleNotFound(Exception exception){
 
-        log.error("Handling not found exception");
+        log.error("Handling not found Exception");
         log.error(exception.getMessage());
 
         ModelAndView modelAndView = new ModelAndView();
@@ -73,4 +73,20 @@ public class RecipeController {
 
         return modelAndView;
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormat(Exception exception){
+
+        log.error("Handling Number Format Exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("400error");
+        modelAndView.addObject("exception", exception);
+
+        return modelAndView;
+    }
+
 }
